967d5b437df5cbd644838992798f59af7ef67276
AznCelebration <ksrithon@UCSD.EDU>
2017-01-19 19:59:53 -0800

    first commit

---
+++ Assets/BuildWall.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildWall : MonoBehaviour {
+
+    public GameObject brick;
+    
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(BrickStack());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator BrickStack() {
+        
+        int numberOfObjects = 54;
+        float radius = 20f;
+        bool mode = true;
+
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            mode = mode ? false : true;
+            for (int j = 0; j < numberOfObjects; j++)
+            {
+                float angle = j * Mathf.PI * 2 / numberOfObjects;
+                if (mode)
+                {
+                    angle += 0.15f;
+                }
+                Vector3 pos = new Vector3(Mathf.Cos(angle), 3, Mathf.Sin(angle)) * radius;
+                Vector3 origin = new Vector3(-pos.x, 3, -pos.z);
+                Instantiate(brick, pos, Quaternion.LookRotation(origin));
+            } 
+        }
+        yield return new WaitForSeconds(5f);
+        var bricks = GameObject.FindGameObjectsWithTag("Brick");
+        foreach (GameObject clone in bricks) {
+            clone.GetComponent<Rigidbody>().constraints = 0;
+        }
+    }
+}
---
+++ Assets/LookControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookControl : MonoBehaviour {
+    public Transform cam;
+    private int gazeDuration = 0;
+    // Use this for initialization
+    void Start () {
+        Debug.DrawRay(cam.position, cam.forward, Color.red, 10.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject prev = null;
+        
+        Ray look = new Ray(cam.position, cam.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(look, out hit, Mathf.Infinity)) {
+            GameObject currLook = hit.collider.gameObject;
+            if (prev == null) {
+                prev = currLook;
+            }
+            if(prev == currLook) {
+                gazeDuration++;
+            }
+            else {
+                prev = currLook;
+                gazeDuration = 0;
+            }
+            if (currLook.tag == "Brick" && gazeDuration == 10) {
+                Destroy(currLook);
+                gazeDuration = 0;
+            }
+        }
+	}
+}
