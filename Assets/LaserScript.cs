4039cefdaf38e4c25e2d26db76acd3e92caf4eef
AznCelebration <azncelebration@gmail.com>
2017-01-24 02:05:06 -0800

    Refined the laser

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -12,32 +12,36 @@ public class LaserScript : MonoBehaviour {
 	void Start () {
         laser.GetComponent<Renderer>().enabled = false;
         particlesys.Pause();
+        particlesys.Clear();
         var main = particlesys.main;
-        main.startDelay = 0;
+        main.startDelay = 0.05f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        laser.SetPositions(positions);
+
     }
 
     void drawLaser(GameObject currLook) {
-        positions[0] = cam.position - currLook.transform.position.normalized * 2;
-        positions[0].y += 0.1f;
-        positions[1] = currLook.transform.position;
-        particlesys.transform.position = cam.position - currLook.transform.position.normalized * 2;
-        particlesys.transform.LookAt(currLook.transform.position);
         StartCoroutine(enableLaser(currLook));
 
 
     }
 
     IEnumerator enableLaser(GameObject currLook) {
-        particlesys.Play();
+        positions[0] = cam.position - currLook.transform.position.normalized * 2;
+        positions[0].y += 0.1f;
+        positions[1] = currLook.transform.position;
+        particlesys.transform.position = cam.position - currLook.transform.position.normalized;
+        particlesys.transform.LookAt(currLook.transform.position);
+        laser.SetPositions(positions);
         laser.GetComponent<Renderer>().enabled = true;
+        particlesys.Play();
+        Vector3 hitpos = currLook.transform.position.normalized * 3;
         yield return new WaitForSeconds(1);
+        positions[0] = cam.position + hitpos;
+        laser.SetPositions(positions);
+        particlesys.Stop();
         laser.GetComponent<Renderer>().enabled = false;
-        particlesys.Pause();
-        particlesys.Clear();
     }
 }
