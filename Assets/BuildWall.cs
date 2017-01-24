69a76ecd61bfc8bd0fef37aa179e52b23bf78ce5
Kavin Srithongkham <ksrithon@UCSD.EDU>
2017-01-23 19:45:41 -0800

    Got cursor working

---
+++ Assets/BuildWall.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildWall : MonoBehaviour {
+    public Texture Reset;
+    public Texture Stack;
+    public GameObject brick;
+    public GameObject status;
+    public bool stacking = false;
+    private List<GameObject> brickList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(BrickStack());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (stacking) {
+            status.GetComponent<Renderer>().material.mainTexture = Stack;
+        }
+        else {
+            status.GetComponent<Renderer>().material.mainTexture = Reset;
+        }
+	}
+
+    void Rebuild()
+    {
+        foreach (GameObject clone in brickList) {
+            Destroy(clone);
+        }
+        brickList.Clear();
+        StartCoroutine(BrickStack());
+
+    }
+
+    IEnumerator BrickStack() {
+        stacking = true;
+        int numberOfObjects = 54;
+        float radius = 20f;
+        bool mode = true;
+        GameObject brickclone;
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            mode = mode ? false : true;
+            for (int j = 0; j < numberOfObjects; j++)
+            {
+                float angle = j * Mathf.PI * 2 / numberOfObjects;
+                if (mode)
+                {
+                    angle += 0.15f;
+                }
+                Vector3 pos = new Vector3(Mathf.Cos(angle), 3, Mathf.Sin(angle)) * radius;
+                Vector3 origin = new Vector3(-pos.x, 3, -pos.z);
+                brickclone = Instantiate(brick, pos, Quaternion.LookRotation(origin));
+                brickList.Add(brickclone);
+            } 
+        }
+        yield return new WaitForSeconds(6f);
+        foreach (GameObject clone in brickList) {
+            clone.GetComponent<Rigidbody>().constraints = 0;
+        }
+        stacking = false;
+    }
+}
---
+++ Assets/LookControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookControl : MonoBehaviour {
+    public Transform cam;
+    public GameObject cannonballPrefab;
+    private GameObject prev = null;
+    private int gazeDuration = 0;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject cannonball;
+        Ray look = new Ray(cam.position, cam.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(look, out hit, Mathf.Infinity)) {
+            GameObject currLook = hit.collider.gameObject;
+            if (currLook.tag == "Interact") {
+                if (prev == null) {
+                    prev = currLook;
+                }
+                if (prev == currLook) {
+                    if (!(currLook.name == "BuildWallControl")) {
+                        gazeDuration++;
+                    }
+                    else if (!currLook.GetComponent<BuildWall>().stacking) {
+                        gazeDuration++;
+                    }
+                }
+                else {
+                    prev = currLook;
+                    gazeDuration = 0;
+                }
+                if (gazeDuration == 100) {
+                    if (currLook.name == "Brick(Clone)") {
+                        cannonball = Instantiate(cannonballPrefab, cam.position, Quaternion.identity);
+                        cannonball.GetComponent<Rigidbody>().velocity = cam.forward.normalized * 50;
+                    }
+
+                    else if (currLook.name == "BuildWallControl") {
+                        if (!currLook.GetComponent<BuildWall>().stacking) {
+                            currLook.SendMessage("Rebuild");
+                        }
+                    }
+                    gazeDuration = 0;
+                }
+                
+            }
+            else {
+                gazeDuration = 0;
+            }
+            cam.GetChild(1).SendMessage("UpdateGaze", gazeDuration);
+        }
+        else {
+            gazeDuration = 0;
+            cam.GetChild(1).SendMessage("UpdateGaze", gazeDuration);
+        }
+	}
+}
