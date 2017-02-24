bf98c811102c23b490c184394591f13a32d46939
Kavin Srithongkham <ksrithon@UCSD.EDU>
2017-02-23 19:02:01 -0800

    Countdown,timer works

--- Project3/Assets/CheckpointControllerScript.cs
+++ Project3/Assets/CheckpointControllerScript.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CheckpointControllerScript : MonoBehaviour {
-
+    public GameObject player;
     public GameObject checkpoint;
 
     private bool[] checkpoints;
+    private GameObject currCheck;
 	// Use this for initialization
 	void Start () {
         int count = 0;
@@ -16,20 +18,37 @@ public class CheckpointControllerScript : MonoBehaviour {
                 string[] tokens = line.Split();
                 GameObject curr = Instantiate(checkpoint, new Vector3((float.Parse(tokens[0])/10), (float.Parse(tokens[1])/10), (float.Parse(tokens[2])/10)),Quaternion.identity);
                 curr.name = count.ToString();
+                if(count == 0) {
+                    player.transform.position = curr.transform.position;
+                }
                 count++;
             }
             checkpoints = new bool[count];
             for (int i = 0; i < checkpoints.Length; i++) checkpoints[i] = false;
+            currCheck = GameObject.FindGameObjectWithTag("Checkpoint");
         }
         catch { print("Load failed"); };
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        this.gameObject.transform.LookAt(currCheck.transform.position);
+        var main = this.gameObject.GetComponent<ParticleSystem>().main;
+        main.startSpeed = Math.Abs(8 - Vector3.Distance(currCheck.transform.position, this.transform.position) * 0.001f);
+
+    }
 
     void newPoint(string num) {
         checkpoints[int.Parse(num)] = true;
+        GameObject[] checks = GameObject.FindGameObjectsWithTag("Checkpoint");
+        float minDist = float.MaxValue;
+        foreach(GameObject point in checks) {
+            if (!checkpoints[int.Parse(point.name)]) {
+                if(Vector3.Distance(point.transform.position,this.transform.position) < minDist) {
+                    minDist = Vector3.Distance(point.transform.position, this.transform.position);
+                    currCheck = point;
+                }
+            }
+        }
     }
 }
--- Project3/Assets/FlyerScript.cs
+++ Project3/Assets/FlyerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Leap;
 using Leap.Unity;
+using UnityEngine.UI;
 
 public class FlyerScript : MonoBehaviour {
     public Transform cam;
@@ -11,18 +12,23 @@ public class FlyerScript : MonoBehaviour {
     public GameObject player;
     public Controller controller;
     public GameObject pointController;
+    public Text count;
+    public Text timer;
 
     private Vector3 direction;
     private LeapProvider provider;
     private Frame frame;
+    private bool start = true;
+    private float time;
     // Use this for initialization
-    void Start () {
+    void Start() {
         direction = new Vector3(1, 0, 0);
         provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        StartCoroutine(Countdown());
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         frame = provider.CurrentFrame;
         if (frame.Hands.Count > 0) {
             List<Hand> hands = frame.Hands;
@@ -43,12 +49,33 @@ public class FlyerScript : MonoBehaviour {
                 Destroy(hits[closest].transform.gameObject);
             }
         }
+        time += Time.deltaTime;
+
+        var minutes = time / 60; //Divide the guiTime by sixty to get the minutes.
+        var seconds = time % 60;//Use the euclidean division for the seconds.
+        var fraction = (time * 100) % 100;
+
+        //update the label value
+        timer.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
     }
 
     private void LateUpdate() {
-        if(frame.Hands.Count > 0) {
+        if (frame.Hands.Count > 0 && !start) {
             //player.transform.LookAt(player.transform.position + direction.normalized, player.transform.up);
             player.transform.position += direction.normalized * 0.5f;
         }
+    }
+    IEnumerator Countdown() {
+        yield return new WaitForSeconds(1f);
+        count.text = "Countdown: 2";
+        yield return new WaitForSeconds(1f);
+        count.text = "Countdown: 1";
+        yield return new WaitForSeconds(1f);
+        count.text = "GO";
+        yield return new WaitForSeconds(1f);
+        count.enabled = false;
+        time = 0f;
+        timer.enabled = true;
+        start = false;
     }
 }
